d9f59b54adf4bbd0ed4d332dcb6100ef6e22ad03
Sophia RK <130664417+s0pa-fria@users.noreply.github.com>
2024-08-01 10:51:28 -0300

    introdução ao python-exemplos

---
+++ 02_variaveis.py
@@ -0,0 +1,17 @@
+# variáveis
+nome = "Bob"
+idade = 12
+
+# Exibição "print"
+print(nome)
+print(idade)
+print("\n")
+
+# 1º método de concatenação (string : srt)
+print("O meu nome é : " + nome + " e tenho " + str(idade) + " anos.\n")
+
+# 2º Método de concatenação (format)
+print("O meu nome é : {} e tenho {} anos.\n" .format(nome, idade))
+
+# 3º Método de concatenação (f string)
+print(f"O meu nome é: {nome} e tenho {idade} anos.\n")
