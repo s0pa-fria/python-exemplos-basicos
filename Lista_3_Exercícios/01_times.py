6c803ba3f92b265905805c69fc67488041ef04f5
Sophia RK <130664417+s0pa-fria@users.noreply.github.com>
2024-08-16 15:23:10 -0300

    fim exercicios

---
+++ Lista_3_Exercícios/01_times.py
@@ -0,0 +1,10 @@
+# Lista usando o laço for
+print("")
+print("======== TIMES ========")
+print("")
+
+times = ["Corinthians", "São Paulo", "Flamengo", "Vasco"]
+
+# Lista usando laço
+for time in times:
+    print(time)
---
+++ Lista_3_Exercícios/02_estados_indice.py
@@ -0,0 +1,10 @@
+# Lista usando o laço for
+print(" ")
+print("Lista dos Estados")
+print(" ")
+
+estados = ["São Paulo", "Rio de Janeiro", "Espirito Santo", "Minas Gerais"]
+
+# Listar usando laço
+for i, estado in enumerate(estados, 1):
+    print(f"{i} - {estado}")
